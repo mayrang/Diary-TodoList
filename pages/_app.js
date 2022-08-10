bb7e8567a3a39f9033f4816af22dbaf59f94b3ed
mayrang <pkss0626@naver.com>
2022-08-10 20:02:10 +0900

    _app, _document 변경, eslint 설정 변경

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = ({Component}) => {
+    return (
+        <>
+            <Component />
+        </>
+    );
+}
+
+App.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+};
+
+export default App;
