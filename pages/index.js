e6945eb670ec150f654efe7177a35e31ba4ce346
mayrang <pkss0626@naver.com>
2022-08-11 11:17:44 +0900

    diary정렬 추가, dummyDiary 추가

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,72 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import HeaderLayout from "../components/HeaderLayout"
-import {Button} from 'antd';
+import {Button, Select} from 'antd';
 import {RightOutlined, LeftOutlined} from '@ant-design/icons';
 import moment from 'moment';
+import styled from 'styled-components';
+
+
+const dummyDiary = [
+    {
+        id: 1,
+        diaryDate: "2022-08-02",
+        diaryContent: "안녕하세요 08월 02일 일기의 더미 데이터입니다^_^",
+        diaryRate: 4.0,
+        diaryWhether: ['Sunny', 'Hot'],
+    },
+    {
+        id: 2,
+        diaryDate: "2022-08-04",
+        diaryContent: "안녕하세요 08월 04일 일기의 더미 데이터입니다..",
+        diaryRate: 2.5,
+        diaryWhether: ['Clody', 'Cool'],
+    },
+    {
+        id: 3, 
+        diaryDate: "2022-08-07",
+        diaryContent: "안녕하세요 08월 07일 일기의 더미 데이터입니다.",
+        diaryRate: 3.5,
+        diaryWhether: ['Rainy', 'Hot'],
+    },
+    {
+        id: 4,
+        diaryDate: "2022-08-09",
+        diaryContent: "안녕하세요 08월 09일 일기의 더미 데이터입니다. 08월의 눈이라니 지구멸망이다!!!",
+        diaryRate: 5.0,
+        diaryWhether: ['Snow', 'Cold'],
+    },
+    {
+        id: 5,
+        diaryDate: "2022-08-10",
+        diaryContent: "안녕하세요 08월 10일 일기의 더미 데이터입니다.",
+        diaryRate: 1.5,
+        diaryWhether: ['Sunny', 'Cool'],
+    }
+
+]
+
+
+
+const SelectWrapper = styled(Select)`
+    display: inline-block;
+    float: right;
+    margin: 1rem;
+`
 
 
 const Home = () => {
     const [month, setMonth] = useState(parseInt(moment().format('MM')));
     const [year, setYear] = useState(parseInt(moment().format('YYYY')));
+    const [sorted, setSorted] = useState("newest");
+    const [sortedPosts, setSortedPosts] = useState([]);
+
+    useEffect(() => {
+        if(sorted === 'newest'){
+            setSortedPosts(dummyDiary.sort((a, b) => parseInt(b.diaryDate.slice(-2)) - parseInt(a.diaryDate.slice(-2))));
+        }else{
+            setSortedPosts(dummyDiary.sort((a, b) => parseInt(a.diaryDate.slice(-2)) - parseInt(b.diaryDate.slice(-2))));
+        }
+    }, [sorted])
 
     const clickPrev = useCallback(() => {
         if(month === 1){
@@ -25,7 +84,11 @@ const Home = () => {
         }else{
             setMonth((prev) => prev + 1)
         }
-    }, [month])
+    }, [month]);
+
+    const handleSelectChange = useCallback((value) => {
+        setSorted(value);
+    }, [])
 
     return (
         <>
@@ -36,6 +99,13 @@ const Home = () => {
         ]} 
         title={"Diary"}
         subTitle={`${year}년 ${month}월`}/>
+        <SelectWrapper
+            onChange={handleSelectChange}
+            value={sorted}
+        >
+            <Select.Option value="latest">최신 순</Select.Option>
+            <Select.Option value="oldest">오래된 순</Select.Option>
+        </SelectWrapper>
         </>
     );
 };
