66855ce8cf732f0ba2bef131008d1536653498fc
mayrang <pkss0626@naver.com>
2022-08-11 18:39:14 +0900

    reducer, store/configureStore 초기 설정, dummyDiary reducer/diary로 이동

---
+++ reducer/diary.js
@@ -0,0 +1,48 @@
+
+
+
+export const dummyDiary = [
+    {
+        id: 1,
+        diaryDate: "2022-08-02",
+        diaryContent: "안녕하세요 08월 02일 일기의 더미 데이터입니다^_^",
+        diaryRate: 4.0,
+        diaryWhether: ['Sunny', 'Hot'],
+    },
+    {
+        id: 2,
+        diaryDate: "2022-08-04",
+        diaryContent: "안녕하세요 08월 04일 일기의 더미 데이터입니다..",
+        diaryRate: 2.5,
+        diaryWhether: ['Clody', 'Cool'],
+    },
+    {
+        id: 3, 
+        diaryDate: "2022-08-07",
+        diaryContent: "안녕하세요 08월 07일 일기의 더미 데이터입니다.",
+        diaryRate: 3.5,
+        diaryWhether: ['Rainy', 'Hot'],
+    },
+    {
+        id: 4,
+        diaryDate: "2022-08-09",
+        diaryContent: "안녕하세요 08월 09일 일기의 더미 데이터입니다. 08월의 눈이라니 지구멸망이다!!!",
+        diaryRate: 5.0,
+        diaryWhether: ['Snow', 'Cold'],
+    },
+    {
+        id: 5,
+        diaryDate: "2022-08-10",
+        diaryContent: "안녕하세요 08월 10일 일기의 더미 데이터입니다.",
+        diaryRate: 1.5,
+        diaryWhether: ['Sunny', 'Cool'],
+    }
+
+]
+
+
+const reducer = (state, action) => {
+
+}
+
+export default reducer;
---
+++ reducer/index.js
@@ -0,0 +1,20 @@
+import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
+import diary from './diary';
+import todo from './todo';
+
+const rootReducer = (state, action) => {
+    switch(action.type){
+        case HYDRATE:
+            return action.payload;
+        default: {
+            const combineReducer = combineReducers({
+                diary,
+                todo
+            });
+            return combineReducer(state, action);
+        }
+    }
+};
+
+export default rootReducer;
